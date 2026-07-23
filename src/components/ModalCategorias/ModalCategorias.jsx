daf248d68e5bc08f9e70e9ae7bd9624cf11f9c2f
Rony Melo <ronymelo15@gmail.com>
2026-07-23 14:00:37 -0300

    fix: iconização categorias

--- src/components/ModalCategorias/ModalCategorias.jsx
+++ src/components/ModalCategorias/ModalCategorias.jsx
@@ -1,12 +1,67 @@
 import { useContext, useEffect, useState } from 'react'
 import { GlobalContext } from '../../hooks/GlobalContext'
+
 import styles from './ModalCategorias.module.scss'
 import categoriaIcon from '../../assets/icons/todas.svg'
+import BeautyIcon from "../../assets/icons/todas.svg";
+import FragrancesIcon from "../../assets/icons/fragrances.svg";
+import FurnitureIcon from "../../assets/icons/furniture.svg";
+import GroceriesIcon from "../../assets/icons/groceries.svg";
+import HomeDecorationIcon from "../../assets/icons/home-decoration.svg";
+import KitchenAccessoriesIcon from "../../assets/icons/kitchen-accessories.svg";
+import LaptopsIcon from "../../assets/icons/laptops.svg";
+import MensShirtsIcon from "../../assets/icons/mens-shirts.svg";
+import MensShoesIcon from "../../assets/icons/mens-shoes.svg";
+import MensWatchesIcon from "../../assets/icons/mens-watches.svg";
+import MobileAccessoriesIcon from "../../assets/icons/mobile-accessories.svg";
+import MotorcycleIcon from "../../assets/icons/motorcycle.svg";
+import SkinCareIcon from "../../assets/icons/skincare.svg";
+import SmartphonesIcon from "../../assets/icons/smartphones.svg";
+import SportsAccessoriesIcon from "../../assets/icons/sports-accessories.svg";
+import SunglassesIcon from "../../assets/icons/sunglasses.svg";
+import TabletsIcon from "../../assets/icons/tablets.svg";
+import TopsIcon from "../../assets/icons/tops.svg";
+import VehicleIcon from "../../assets/icons/vehicle.svg";
+import WomensBagsIcon from "../../assets/icons/bags.svg";
+import WomensDressesIcon from "../../assets/icons/womens-dresses.svg";
+import WomensJewelleryIcon from "../../assets/icons/jewellery.svg";
+import WomensShoesIcon from "../../assets/icons/womens-shoes.svg";
+import WomensWatchesIcon from "../../assets/icons/womens-watches.svg";
 
 const ModalCategorias = ({ openModal, setOpenModal }) => {
     const [categorias, setCategorias] = useState([])
+    const [catVisiveis, setCatVisiveis] = useState(16)
     const global = useContext(GlobalContext)
 
+    const categoryIcones = {
+        beauty: BeautyIcon,
+        fragrances: FragrancesIcon,
+        furniture: FurnitureIcon,
+        groceries: GroceriesIcon,
+        "home-decoration": HomeDecorationIcon,
+        "kitchen-accessories": KitchenAccessoriesIcon,
+        laptops: LaptopsIcon,
+        "mens-shirts": MensShirtsIcon,
+        "mens-shoes": MensShoesIcon,
+        "mens-watches": MensWatchesIcon,
+        "mobile-accessories": MobileAccessoriesIcon,
+        motorcycle: MotorcycleIcon,
+        "skin-care": SkinCareIcon,
+        smartphones: SmartphonesIcon,
+        "sports-accessories": SportsAccessoriesIcon,
+        sunglasses: SunglassesIcon,
+        tablets: TabletsIcon,
+        tops: TopsIcon,
+        vehicle: VehicleIcon,
+        "womens-bags": WomensBagsIcon,
+        "womens-dresses": WomensDressesIcon,
+        "womens-jewellery": WomensJewelleryIcon,
+        "womens-shoes": WomensShoesIcon,
+        "womens-watches": WomensWatchesIcon,
+    };
+
+    console.log(categoryIcones)
+    
     // Consumindo api que tem as catedorias, e colocando na variavel Categorias
     useEffect(() => {
         fetch("https://dummyjson.com/products/category-list")
@@ -20,12 +75,10 @@ const ModalCategorias = ({ openModal, setOpenModal }) => {
     // Função que pega o valor da categoria clicada
     function handleCategoria(event) {
         global.setCategoria(event.target.innerText)
+        setOpenModal(false)
+
     }
-
-
-
     if (!openModal) return null
-
     return (
         <>
             <div className={styles.modalOverlay}></div>
@@ -36,26 +89,26 @@ const ModalCategorias = ({ openModal, setOpenModal }) => {
                         <h2>Todas as categorias</h2>
                         <p>Encontre o que você precisa em todas as nossas categorias.</p>
                     </div>
-                    <button onClick={() => setOpenModal(false)}>Fechar modal</button>
+
 
                     <div
-                        onClick={global.closeModal}
+                        onClick={() => setOpenModal(false)}
                         className={styles.modal__close}
                     ></div>
                 </div>
 
                 <div className={styles.categorias__list}>
 
-                    {categorias?.map((categoria, index) => {
+                    {categorias?.slice(0, catVisiveis).map((categoria, index) => {
                         return (
                             <button
                                 key={index}
                                 className={styles.categorias__item}
                                 onClick={handleCategoria}
                             >
-                                <img src={categoriaIcon} alt="" className={styles.categorias__icon} />
+                                <img src={categoryIcones[categoria]} alt={categoria} className={styles.categorias__icon} />
                                 <span className={styles.categorias__nome}>{categoria}</span>
-                                <span className={styles.categorias__quantidade}>50 produtos</span>
+
                             </button>
                         )
 
@@ -63,7 +116,7 @@ const ModalCategorias = ({ openModal, setOpenModal }) => {
 
                 </div>
 
-                <button className={styles.modalCategorias__more}>
+                <button onClick={() => setCatVisiveis(catVisiveis + 4)} className={styles.modalCategorias__more}>
                     Carregar mais categorias
                 </button>
             </div>
